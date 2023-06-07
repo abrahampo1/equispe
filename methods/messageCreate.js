4d1d5f78e232e94a0b85014353e4a357b0feb44a
Abraham Leiro Fernández <abrahamleirofernandez@gmail.com>
2023-06-07 07:39:29 +0200

    🚑  Fixed bad access to guild id variable

--- methods/messageCreate.js
+++ methods/messageCreate.js
@@ -6,30 +6,31 @@ async function messageCreate(message) {
     return;
   }
 
+  const guild = message.guildId;
   const author = message.author.id;
   const time = message.createdTimestamp;
   const xp = message.content.length;
   let total_xp = 0;
   let id = "nextval('users_id_seq'::regclass)";
   const data = await pool.query(
-    `SELECT * FROM public.users WHERE userid = '${author}' and guild = ${message.author.guild.id}`
+    `SELECT * FROM public.users WHERE userid = '${author}' and guild = ${guild}`
   );
   if (data.rows.length == 0) {
     total_xp = xp;
   } else {
     id = data.rows[0].id;
     total_xp = parseInt(xp) + parseInt(data.rows[0].xp);
-    if (Date.now() - (cooldown * 1000) < parseInt(data.rows[0].last_message)) {
+    if (Date.now() - cooldown * 1000 < parseInt(data.rows[0].last_message)) {
       return;
     }
   }
 
   pool.query(
-    `INSERT INTO public.users (id, userid, xp, last_message, guild) VALUES(${id}, '${author}', ${total_xp}, ${time}, ${message.member.guild.id}) ON CONFLICT (id) DO UPDATE SET xp=${total_xp}, last_message=${time}`
+    `INSERT INTO public.users (id, userid, xp, last_message, guild) VALUES(${id}, '${author}', ${total_xp}, ${time}, ${guild}) ON CONFLICT (id) DO UPDATE SET xp=${total_xp}, last_message=${time}`
   );
 
   const rol_query = await pool.query(
-    `SELECT * FROM roles WHERE xp <= ${total_xp} and guild = ${message.author.guild.id} ORDER BY xp DESC`
+    `SELECT * FROM roles WHERE xp <= ${total_xp} and guild = ${guild} ORDER BY xp DESC`
   );
 
   if (rol_query.rows.length == 0) {
